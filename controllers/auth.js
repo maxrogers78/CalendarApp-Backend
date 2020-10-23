8759a24731ea9718e6da09839cd38269c15df3e6
Max <maxrogerssepulveda@gmail.com>
2020-10-23 17:41:05 -0300

    body request referenced

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,16 +2,32 @@
 const { response } = require("express");
 
 const createUser = (req, res = response) => {
+  const { name, email, password } = req.body;
+
+  if (name.length < 5) {
+    return res.status(400).json({
+      ok: false,
+      msg: "El largo del nombre debe ser mayor a 5 letras",
+    });
+  }
+
   res.json({
     ok: true,
     msg: "registro",
+    name,
+    email,
+    password,
   });
 };
 
 const loginUser = (req, res = response) => {
+  const { email, password } = req.body;
+
   res.json({
     ok: true,
     msg: "login",
+    email,
+    password,
   });
 };
 
