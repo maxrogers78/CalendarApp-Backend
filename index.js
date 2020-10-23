d0bb824d06257463085492ffe84e3743cfc24312
Max <maxrogerssepulveda@gmail.com>
2020-10-23 14:13:14 -0300

    express basic config created

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Hello World!");
+const express = require("express");
+
+// Crear el servidor de express
+const app = express();
+
+// Rutas
+app.get("/", (req, res) => {
+  res.json({
+    ok: true,
+  });
+});
+
+// Escuchar peticiones
+app.listen(4000, () => {
+  console.log("Servidor en puerto 4000");
+});
