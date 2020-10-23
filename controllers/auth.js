d607e22e78fecf3aed9c7687bba18ea56fd35efe
Max <maxrogerssepulveda@gmail.com>
2020-10-23 20:01:34 -0300

    Save user functionality added

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,17 +2,28 @@
 const { response } = require("express");
 // Express-validator
 const { validationResult } = require("express-validator");
+// Models
+const Usuario = require("../models/Usuario");
 
-const createUser = (req, res = response) => {
-  const { name, email, password } = req.body;
+const createUser = async (req, res = response) => {
+  // const { name, email, password } = req.body;
 
-  res.status(201).json({
-    ok: true,
-    msg: "registro",
-    name,
-    email,
-    password,
-  });
+  try {
+    const usuario = new Usuario(req.body);
+
+    await usuario.save();
+
+    res.status(201).json({
+      ok: true,
+      msg: "registro",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: "Por favor hable con el administrador",
+    });
+  }
 };
 
 const loginUser = (req, res = response) => {
