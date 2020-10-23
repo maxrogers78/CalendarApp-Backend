a0436de01d30fed8e2323099c046040654fea173
Max <maxrogerssepulveda@gmail.com>
2020-10-23 14:35:35 -0300

    api/auth route added

--- index.js
+++ index.js
@@ -8,11 +8,8 @@ const app = express();
 app.use(express.static("public"));
 
 // Rutas
-/* app.get("/", (req, res) => {
-  res.json({
-    ok: true,
-  });
-}); */
+app.use("/api/auth", require("./routes/auth"));
+// TODO: CRUD: Eventos
 
 // Escuchar peticiones
 app.listen(process.env.PORT, () => {
