efbb658ff6c0ea363e49abf6c38e8ed3efef77bc
Max <maxrogerssepulveda@gmail.com>
2020-10-23 20:10:52 -0300

    email validation added

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,16 +6,27 @@ const { validationResult } = require("express-validator");
 const Usuario = require("../models/Usuario");
 
 const createUser = async (req, res = response) => {
-  // const { name, email, password } = req.body;
+  const { email, password } = req.body;
 
   try {
-    const usuario = new Usuario(req.body);
+    let usuario = await Usuario.findOne({ email });
+    console.log(usuario);
+
+    if (usuario) {
+      return res.status(400).json({
+        ok: false,
+        msg: "Un usuario ya existe con ese correo",
+      });
+    }
+
+    usuario = new Usuario(req.body);
 
     await usuario.save();
 
     res.status(201).json({
       ok: true,
-      msg: "registro",
+      uid: usuario.id,
+      name: usuario.name,
     });
   } catch (error) {
     console.log(error);
